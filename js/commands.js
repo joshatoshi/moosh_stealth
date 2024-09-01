b69cb6692bc57bce9bbc308bf5b867e1f8ca273c
joshatoshi <158278249+joshatoshi@users.noreply.github.com>
2024-09-01 02:21:26 -0700

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,71 @@
+var twitter = "https://www.x.com/JoshCowellX";
+var linkedin = "https://www.linkedin.com/in/jcowell/";
+var github = "https://github.com/joshatoshi/";
+var email = 'mailto:admin@moosh.gg';
+
+whois = [
+  "<br>",
+  "Hello World!👋👀",
+  "Moosh is decentralized peer-to-peer (P2P) networking project. We are currently is stealth mode.",
+  "We believe that upgrading today's internet (Web2) requires an upgrade to the mesh topology",
+  "and we love open networks so much we got a domain with two oh's, double the openness!",
+  "How open are we? Two O's worth of mesh. Mesh + oo = Moosh.",
+  "Blockchains are trustless, human's...are not.",
+  "Blockchains can be used for trustless machine communication, and bring to fruitiion immutable data.",
+  "(1) Moosh uses blockchain to link together highly trusted machines for resource sharing.",
+  "(2) and also to store on-chain performance history of each of their human peer counterparts.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'twitter/X      <a href="' + twitter + '" target="_blank">twitter/JoshCowellX' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/jcowell' + "</a>",
+  'github         <a href="' + github + '" target="_blank">github/joshatoshi' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Are you the admin though?',
+  "<br>"
+];
+
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          What is Moosh?',
+  '<span class="command">social</span>         Display social links',
+  '<span class="command">help</span>           Really? You already used this buddy...',
+  '<span class="command">email</span>          Contact us',
+  '<span class="command">clear</span>          Clear the terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Moosh network is an open source project. Stealth-mode:: active.</span>',
+  "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                                                                                                     ",
+  "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                                                                                                     ",
+  "@@@@@@@@@@@@@@@@@@@@@@@@.             .@@@@@@@@@@@@@@@.    (@@@@@@@@@@@@@@@@@@@@                                                                           hhhhhhh                   ",
+  "@@@@@@@@@@@@@@@@@@@@@%.                  *@@@@@@@@@@@@@@/      @@@@@@@@@@@@@@@@@                                                                           h:::::h                   ",
+  "@@@@@@@@@@@@@@@@@@@@                       @@@@@@@@@@@@@@@.      @@@@@@@@@@@@@@@                                                                           h:::::h                   ",
+  "@@@@@@@@@@@@@@@@@@@.                        @@@@@@@@@@@@@@@.      @@@@@@@@@@@@@@                                                                           h:::::h                   ",
+  "@@@@@@@@@@@@@@@@@@@                         @@@@@@@@@@@@@@@@      /@@@@@@@@@@@@@        mmmmmmm    mmmmmmm      ooooooooooo      ooooooooooo       ssssssssss    h::::h hhhhh        ",
+  "@@@@@@@@@@@@@@@@@@@                         @@@@@@@@@@@@@@@@      (@@@@@@@@@@@@@      mm:::::::m  m:::::::mm  oo:::::::::::oo  oo:::::::::::oo   ss::::::::::s   h::::hh:::::hhh     ",
+  "@@@@@@@@@@@@@@@@@@@@                       &@@@@@@@@@@@@@@@.      @@@@@@@@@@@@@@     m::::::::::mm::::::::::mo:::::::::::::::oo:::::::::::::::oss:::::::::::::s  h::::::::::::::hh   ",
+  "@@@@@@@@@@@@@@@@@@@@@.                    @@@@@@@@@@@@@@@@      ,@@@@@@@@@@@@@@@     m::::::::::::::::::::::mo:::::ooooo:::::oo:::::ooooo:::::os::::::ssss:::::s h:::::::hhh::::::h  ",
+  "@@@@@@@@@@@@@@@@@@@@@@@%               /@@@@@@@@@@@@@@@@      *@@@@@@@@@@@@@@@@@     m:::::mmm::::::mmm:::::mo::::o     o::::oo::::o     o::::o s:::::s  ssssss  h::::::h   h::::::h ",
+  "@@@@@@@@@@@@@@@@@@@@@@@@@@@@#*...,#@@@@@@@@@@@@@@@@@@@.  .(@@@@@@@@@@@@@@@@@@@@@     m::::m   m::::m   m::::mo::::o     o::::oo::::o     o::::o   s::::::s       h:::::h     h:::::h ",
+  "@@@@@@@@@@@@@@@@@@@@@@@%,             ,#@@@@@@@@@@@@@@*****(%@@@@@@@@@@@@@@@@@@@     m::::m   m::::m   m::::mo::::o     o::::oo::::o     o::::o      s::::::s    h:::::h     h:::::h ",
+  "@@@@@@@@@@@@@@@@@@.                         .@@@@@@@@@.           *@@@@@@@@@@@@@     m::::m   m::::m   m::::mo::::o     o::::oo::::o     o::::ossssss   s:::::s  h:::::h     h:::::h ",
+  "@@@@@@@@@@@@@@@                                 @@@@@@.              .@@@@@@@@@@     m::::m   m::::m   m::::mo:::::ooooo:::::oo:::::ooooo:::::os:::::ssss::::::s h:::::h     h:::::h ",
+  "@@@@@@@@@@@@@                                     &@@@.                 &@@@@@@@     m::::m   m::::m   m::::mo:::::::::::::::oo:::::::::::::::os::::::::::::::s  h:::::h     h:::::h ",
+  "@@@@@@@@@@@*                                       .@@@(                 .@@@@@@     m::::m   m::::m   m::::m oo:::::::::::oo  oo:::::::::::oo  s:::::::::::ss   h:::::h     h:::::h ",
+  "@@@@@@@@@@,                                          @@@#                  @@@@@     mmmmmm   mmmmmm   mmmmmm   ooooooooooo      ooooooooooo     sssssssssss     hhhhhhh     hhhhhhh ",
+  "@@@@@@@@@&                                           #@@@                  (@@@@                                                                                                     ",
+  "@@@@@@@@@/                                            @@@.                  @@@@                                                                                                     ",
+  "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                                                                                                     ",
+  '<span class="color2">Welcome to the Moosh web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
